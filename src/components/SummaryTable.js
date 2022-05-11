cd90e70f5376b8eb44885150027d76cf2ccc5a27
thucnhu <buitranthucnhu@gmail.com>
2022-05-12 00:02:48 +0700

    make UI responsive

--- src/components/SummaryTable.js
+++ src/components/SummaryTable.js
@@ -1,5 +1,10 @@
 import React, { useState } from 'react'
 
 export default function SummaryTable() {
-	return <div>Summary Table</div>
+	return (
+		<div className='bg-brand-grey w-56 h-64 rounded-md p-4 flex flex-col items-center justify-evenly'>
+			<h5>Summary Table</h5>
+			{/* TODO: Display graph data structure here */}
+		</div>
+	)
 }
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react'
-import { GraphBoard, VisualizeTable } from '../components'
+import { GraphBoard, VisualizeTable, SummaryTable } from '../components'
 import Button from 'react-bootstrap/Button'
 
 export default function Home() {
@@ -8,12 +8,12 @@ export default function Home() {
 
 	return (
 		<div
-			className='w-full flex md:flex-row flex-col justify-between items-center border-2 border-black'
-			style={{ height: '92vh' }}
+			className='w-full flex md:flex-row flex-col justify-between items-center'
+			style={{ height: '92vh', minHeight: '600px' }}
 		>
-			<div className='flex flex-col w-3/4 h-5/6 justify-center'>
+			<div className='flex flex-col md:w-4/5 w-11/12 h-5/6 justify-center'>
 				<GraphBoard />
-				<div className='flex flex-row justify-center mt-3'>
+				<div className='flex flex-row justify-center my-3'>
 					<Button
 						variant='secondary'
 						disabled={removeDisabled}
@@ -30,7 +30,8 @@ export default function Home() {
 					</Button>
 				</div>
 			</div>
-			<div className='flex flex-col justify-center'>
+			<div className='flex md:flex-col sm:flex-row justify-between md:h-5/6 sm:h-auto w-full md:w-fit'>
+				<SummaryTable />
 				<VisualizeTable />
 			</div>
 		</div>
