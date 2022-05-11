2a3f6b1b10b3dd6ceae047611dd8c6cb93e8e225
ngoclong-fulbright <69509154+ngoclong-fulbright@users.noreply.github.com>
2022-05-12 03:17:45 +0700

    on click add node to graph board

--- src/components/GraphBoard.js
+++ src/components/GraphBoard.js
@@ -3,15 +3,18 @@ import ReactFlow, { Controls, Background } from 'react-flow-renderer'
 import {
 	applyEdgeChanges,
 	applyNodeChanges,
-	addEdge,
+	addEdge
 } from 'react-flow-renderer'
 import { initialEdges, initialNodes } from '../constant/initialGraph'
+import $ from 'jquery';
 
 import './GraphBoard.css'
 
 export default function GraphBoard() {
 	const [nodes, setNodes] = useState(initialNodes)
 	const [edges, setEdges] = useState(initialEdges)
+	const [graph, setGraph] = useState(null);
+	const [pressed, setPressed] = useState(false)
 
 	const onNodesChange = useCallback(
 		changes => setNodes(nds => applyNodeChanges(changes, nds)),
@@ -27,20 +30,87 @@ export default function GraphBoard() {
 		connection => setEdges(eds => addEdge(connection, eds)),
 		[setEdges]
 	)
+	
+	const onGraphInit = (instance) => {
+		setGraph(instance)
+	}
+	
+	const getZoomFactor = () => {
+		let node1 = $('#graph').find('[data-id="1"]')[0].getBoundingClientRect()
+		let node2 = $('#graph').find('[data-id="2"]')[0].getBoundingClientRect()
 
+		let nodeG1 = graph.getNode('1')
+		let nodeG2 = graph.getNode('2')
+
+		return (node1.x - node2.x)/(nodeG1.position.x - nodeG2.position.x)
+	}
+
+	const onBoardClick = (e) => {
+		if (!pressed) {
+			let zoomFactor = getZoomFactor()
+
+			let baseNodeDom = $('#graph').find('[data-id="1"]')[0]
+			let baseNodeRect = baseNodeDom.getBoundingClientRect()
+			
+			let nodeScreenX = baseNodeRect.x + baseNodeRect.width/2
+			let nodeScreenY = baseNodeRect.y + baseNodeRect.height/2
+
+			let newX = (e.clientX - nodeScreenX)/zoomFactor
+			let newY = (e.clientY - nodeScreenY)/zoomFactor
+			let newId = (graph.getNodes().length+1).toString()
+
+			graph.addNodes(
+				{
+					id: newId,
+					data: {label: newId},
+					position: { x: newX, y: newY },
+				},
+			)
+		}
+
+		// reset add node listener
+		setPressed(false)
+	}
+
+	// hanlde mouse drag -> dont add node
+	const onBoardMouseDown = () => {
+		setPressed(true)
+	}
+
+	const test = () => {
+		graph.addNodes(
+			{
+			id: (graph.getNodes().length+1).toString(),
+			data: { label: <div>E</div> },
+			position: { x: 0, y: 125 },
+			}
+		)
+		graph.addEdges(
+			{ id: 'e5-2', source: '5', target: '2', label: '45' }
+		)
+	}
+
+	
 	return (
-		<div className='border-2 border-black h-full min-w-[30rem] w-full rounded-md'>
-			<ReactFlow
-				nodes={nodes}
-				edges={edges}
-				onNodesChange={onNodesChange}
-				onEdgesChange={onEdgesChange}
-				onConnect={onConnect}
-				fitView
-			>
-				<Controls />
-				<Background />
-			</ReactFlow>
+		<div className=' h-full min-w-[30rem] w-full flex flex-col items-end'>
+				<div id='graph' className='w-full h-full border-2 border-black rounded-md' 
+					onMouseDown={onBoardMouseDown}
+					onClick={onBoardClick}>
+					<ReactFlow
+						nodes={nodes}
+						edges={edges}
+						onNodesChange={onNodesChange}
+						onEdgesChange={onEdgesChange}
+						onConnect={onConnect}
+						onInit={(instance) => onGraphInit(instance)}
+						fitView
+					>
+						<Controls />
+						<Background />
+					</ReactFlow>
+				</div>
+
+				<button className='btn-green mt-5' onClick={test}>Test</button>
 		</div>
 	)
 }
