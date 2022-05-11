758c27da2cdc5a9a99404bcd26016225f5be8d37
ngoclong-fulbright <69509154+ngoclong-fulbright@users.noreply.github.com>
2022-05-11 16:17:33 +0700

    graph react-flow init

---
+++ src/const/InitialGraph.js
@@ -0,0 +1,28 @@
+const initialNodes = [
+  {
+    id: '1',
+    // type: 'input',
+    data: { label: 'A' },
+    position: { x: 250, y: 25 },
+  },
+  
+  {
+    id: '2',
+    // you can also pass a React component as a label
+    data: { label: <div>B</div> },
+    position: { x: 100, y: 125 },
+  },
+  {
+    id: '3',
+    // type: 'output',
+    data: { label: 'C' },
+    position: { x: 250, y: 250 },
+  },
+];
+
+const initialEdges = [
+  { id: 'e1-2', source: '1', target: '2' },
+  { id: 'e2-3', source: '2', target: '3' },
+];
+
+export {initialEdges, initialNodes}
