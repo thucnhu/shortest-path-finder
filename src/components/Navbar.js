83a9d3c4400d42e09903f8913ffd0b1d31630a5f
ngoclong-fulbright <69509154+ngoclong-fulbright@users.noreply.github.com>
2022-05-15 14:34:46 +0700

    logo link

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -7,7 +7,7 @@ export default function NavigationBar() {
 	return (
 		<Navbar bg='light' expand='lg'>
 			<Container>
-				<Navbar.Brand href='#home'>Shortest Path Finder</Navbar.Brand>
+				<Navbar.Brand href='/'>Shortest Path Finder</Navbar.Brand>
 				<Navbar.Toggle aria-controls='basic-navbar-nav' />
 				<Navbar.Collapse id='basic-navbar-nav'>
 					<Nav className='me-auto'>
